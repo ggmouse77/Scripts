ab5198a32835a7b9d012f014894532612ce78e18
ggmouse77 <ggmouse77@gmail.com>
2025-08-28 21:00:58 +0900

    Update rhel810_local_check.sh

--- rhel810_local_check.sh
+++ rhel810_local_check.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-PKG_DIR="/root/repo"
-LOG="/root/update_check.log"
+PKG_DIR="/user/svrauto/SA/OS/2025PM/rhel810_local_rpms"
+LOG="/home/update_check.log"
 
-dnf --disablerepo='*' localinstall ${PKG_DIR}/*.rpm --assumeno > "$LOG" 2>&1
+{
+  dnf --disablerepo='*' --setopt=tsflags=test -y localinstall "${PKG_DIR}"/*.rpm
+  RC=$?
+
+  echo "--- dnf localinstall(test) end rc=${RC} ---"
+  exit $RC
+} >"$LOG" 2>&1
